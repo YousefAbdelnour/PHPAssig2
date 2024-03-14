9ec32f3a5177e9e6755bf76240609153b97ad9e2
Yousef Abdelnour <youssefabdelnour2004@yahoo.com>
2024-03-14 14:21:40 -0400

    finished publication model, controller to be done

---
+++ app/models/Publication.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace app\models;
+
+use PDO;
+
+class Publication extends \app\core\Model
+{
+    public $id;
+    public $profile_id;
+    public $title;
+    public $text;
+    public $timestamp;
+    public $status;
+
+    public function create()
+    {
+        $SQL = 'INSERT INTO publication(profile_id,publication_title,publication_text,timestamp,publication_status)
+        VALUE (:profile_id,:publication_title,:publication_text,:timestamp,:publication_status)';
+        $STMT = self::$_conn->prepare($SQL);
+        $STMT->execute(
+            [
+                'profile_id' => $this->profile_id,
+                'publication_title' => $this->title,
+                'publication_text' => $this->text,
+                'timestamp' => $this->timestamp,
+                'publication_status' => $this->status
+            ]
+        );
+    }
+    public function getAll()
+    {
+        $SQL = 'SELECT * FROM publication WHERE publication_status = :publication_status';
+        $STMT = self::$_conn->prepare($SQL);
+        $STMT->execute(['publication_status' => true]);
+        $STMT->setFetchMode(PDO::FETCH_CLASS, 'app\models\Publication');
+        return $STMT->fetchAll();
+    }
+    public function getByTitle($pTitle)
+    {
+        $SQL = 'SELECT * FROM publication WHERE publication_title = :publication_title AND publication_status = :publication_status';
+        $STMT = self::$_conn->prepare($SQL);
+        $STMT->execute(
+            [
+                'publication_title' => $pTitle,
+                'publication_status' => true
+            ]
+        );
+        $STMT->setFetchMode(PDO::FETCH_CLASS, 'app\models\Publication');
+        return $STMT->fetchAll();
+    }
+    public function getByContent($pContent)
+    {
+        $SQL = 'SELECT * FROM publication WHERE publication_text = :publication_text AND publication_status = :publication_status';
+        $STMT = self::$_conn->prepare($SQL);
+        $STMT->execute(
+            [
+                'publication_text' => $pContent,
+                'publication_status' => true
+            ]
+        );
+        $STMT->setFetchMode(PDO::FETCH_CLASS, 'app\models\Publication');
+        return $STMT->fetchAll();
+    }
+    public function getForOwner()
+    {
+        $SQL = 'SELECT * FROM publication WHERE profile_id = :profile_id';
+        $STMT = self::$_conn->prepare($SQL);
+        $STMT->execute(['profile_id' => $this->profile_id]);
+        $STMT->setFetchMode(PDO::FETCH_CLASS, 'app\models\Publication');
+        return $STMT->fetchAll();
+    }
+    public function edit()
+    {
+        $SQL = 'UPDATE publication SET publication_title=:publication_title,
+        timestamp=:timestamp,
+        publication_status=:publication_status,
+        publication_text=:publication_text 
+        WHERE publication_id = :publication_id';
+        $STMT = self::$_conn->prepare($SQL);
+        $STMT->execute(
+            [
+                'publication_id' => $this->id,
+                'publication_title' => $this->title,
+                'timestamp' => $this->timestamp,
+                'publication_status' => $this->status,
+                'publication_text' => $this->text
+            ]
+        );
+    }
+    public function delete()
+    {
+        $SQL = 'DELETE FROM publication WHERE publication_id = :publication_id';
+        $STMT = self::$_conn->prepare($SQL);
+        $STMT->execute(
+            ['publication_id' => $this->id]
+        );
+    }
+}
